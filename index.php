3f5c9d2b518f441fb368719fadb21fe0d838eaf8
ruby duran <ruby duran@gmail.com>
2016-02-29 17:48:33 +0100

    mi 1.gif

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<img src="1.gif" width="200" alt="">
+	<marquee>
+		<h1>lidia rubith duran delgado</h1>
+
+	</marquee>
+</body>
+</html>
